11f0c9ac4149a2964e4f809b5288ba9a017511e4
uguryesilyurt <uguryesilyurt@hotmail.com.tr>
2021-01-05 21:16:27 +0300

    Initial commit for our Spring Boot Reddit Clone

---
+++ src/main/java/com/ugur/springit/SpringItApplication.java
@@ -0,0 +1,13 @@
+package com.ugur.springit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringItApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringItApplication.class, args);
+    }
+
+}
