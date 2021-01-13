29e7fdae45a9ce0b41f4629aa3d4ebadd553c690
uguryesilyurt <uguryesilyurt@hotmail.com.tr>
2021-01-13 22:54:04 +0300

    Adding Logger to SpringItApplication

--- src/main/java/com/ugur/springit/SpringItApplication.java
+++ src/main/java/com/ugur/springit/SpringItApplication.java
@@ -1,10 +1,18 @@
 package com.ugur.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringItApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringItApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringItApplication.class, args);
