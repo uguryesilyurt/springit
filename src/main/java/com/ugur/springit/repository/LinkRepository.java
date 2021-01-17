96797750c30c35909f198b0ea62ad59f9e589ee3
uguryesilyurt <uguryesilyurt@hotmail.com.tr>
2021-01-17 23:29:35 +0300

    unused import deleted

--- src/main/java/com/ugur/springit/repository/LinkRepository.java
+++ src/main/java/com/ugur/springit/repository/LinkRepository.java
@@ -3,8 +3,6 @@ package com.ugur.springit.repository;
 import com.ugur.springit.domain.Link;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface LinkRepository extends JpaRepository<Link, Long> {
 
     Link findByTitle(String title);
