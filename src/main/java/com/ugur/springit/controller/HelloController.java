5186f6478dd1f9e242c8046b538c6514200c4008
uguryesilyurt <uguryesilyurt@hotmail.com.tr>
2021-01-19 22:15:05 +0300

    Rest controller created

---
+++ src/main/java/com/ugur/springit/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.ugur.springit.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String handle(Model model) {
+        model.addAttribute("message", "Hello World!");
+        return "index";
+    }
+
+}
